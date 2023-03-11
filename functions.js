22bb185aaf9cc03254988d7bb1641bcf8316ace0
z-guzman <zbguzman@up.edu.ph>
2023-03-11 13:57:23 +0800

    docs: added documentation

--- functions.js
+++ functions.js
@@ -3,23 +3,23 @@ import validator from 'validator';
 import fs from 'fs';
 
 function generateUniqueID(firstname, lastname){
-    var generatedID = uuidv4();
-    var id = generatedID.slice(0, 8);
-    return (firstname[0].toLowerCase() + lastname.toLowerCase() + id);
+    var generatedID = uuidv4(); // Generates id using the uuid import
+    var id = generatedID.slice(0, 8); // Shortens the generated id to 8 characters
+    return (firstname[0].toLowerCase() + lastname.toLowerCase() + id); // Returns concatenated id string 
 }
 
 function addAccount (details){
-    var isValid = false;
-    if (details.length == 4){
-        if (typeof details[0] === 'string' && typeof details[1] === 'string' && typeof details[2] === 'string'){
-            if (details[0] && details[1] && details[2]){
-                if (validator.isEmail(details[2])){
-                    if (details[3] >= 18){
-                        isValid = true;
-                        var newID = generateUniqueID(details[0], details[1]);
-                        details.push(newID);
-                        const stringedDetails = JSON.stringify(details);
-                        fs.appendFileSync('users.txt', (stringedDetails + "\n"))
+    var isValid = false; // Credentials are marked false by default
+    if (details.length == 4){ // Checks if array given contains exactly four fields
+        if (typeof details[0] === 'string' && typeof details[1] === 'string' && typeof details[2] === 'string'){ // Checks type of the first three elements of the array
+            if (details[0] && details[1] && details[2]){ // Checks if the first three elements of the array are non-empty
+                if (validator.isEmail(details[2])){ // Validates email format using the validator import
+                    if (details[3] >= 18){ // Checks if age is equal or above 18
+                        isValid = true; // Marks the given credentials as valid
+                        var newID = generateUniqueID(details[0], details[1]); // Creates new ID using the generateUniqueID function
+                        details.push(newID); // Appends the ID to the given array
+                        const stringedDetails = JSON.stringify(details); // Turns array into string
+                        fs.appendFileSync('users.txt', (stringedDetails + "\n")) // Appends details to file
                     }
                 }
             }
@@ -27,7 +27,7 @@ function addAccount (details){
     }
 
     console.log(isValid);
-    return isValid;
+    return isValid; // Returns boolean status of credentials
 }
 
-export default { generateUniqueID, addAccount };
+export default { generateUniqueID, addAccount }; // Exports to test file
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-import functions from "./functions.js";
+import functions from "./functions.js"; // Imports functions from function file
 
-functions.addAccount(["Alan", "Turing", "aturing@w3c.com"]);
-functions.addAccount([69, "Turing", "aturing@w3c.com", 58]);
-functions.addAccount(["", "Turing", "aturing@w3c.com", 58]);
-functions.addAccount(["Alan", "Turing", "aturingw3c.com", 58]);
-functions.addAccount(["", "Turing", "aturing@w3c.com", 16]);
+// False
+functions.addAccount(["Alan", "Turing", "aturing@w3c.com"]); //False: Not enough fields have been filled out
+functions.addAccount(["Alan", "Turing", "aturing@w3c.com", 58, "British"]); //False: Too many fields
+functions.addAccount([69, "Turing", "aturing@w3c.com", 58]); //False: First name is not a string
+functions.addAccount(["Alan", "", "aturing@w3c.com", 58]); //False: Last name is an empty string
+functions.addAccount(["Alan", "Turing", "aturingw3c.com", 58]); //False: Email is not in proper format
+functions.addAccount(["", "Turing", "aturing@w3c.com", 16]); //False: Age is under 18
 
+// True
 functions.addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
 functions.addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]);
 functions.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]);
